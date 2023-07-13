bc99cb8bb6208a40ce28f059058dcba0b2ff5dea
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-13 20:11:47 +0000

    Task 1: In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- SQL script that creates a table users
+-- It should contain id, name and email
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
+);
