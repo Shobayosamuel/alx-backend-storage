a36613255051355086d9d417f53d68c1cf97ea23
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-13 22:12:33 +0000

    fix: syntax erro for task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email varchar(255) NOT NULL UNIQUE,
 	name varchar(255),
-	country ENUM ('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
+	country ENUM ('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
