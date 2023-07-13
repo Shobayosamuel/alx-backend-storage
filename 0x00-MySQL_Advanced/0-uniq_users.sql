901afbae1ff1f4590ae7ae1afc76d21eb56fc885
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-13 19:08:16 +0000

    tsk 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- SQL script that creates a table users
+-- It should contain id, name and email
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+);
