6b09aef1e839659688f3746a5b16de3159641dbd
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-14 13:47:55 +0000

    fix syntax error

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Write a SQL script that ranks country origins of bands
 -- ordered by the number of (non-unique) fans
-SELECT origin, SUM(fans) as n_of_fans 
+SELECT origin, SUM(fans) AS n_of_fans 
     FROM metal_bands
     GROUP BY origin
     ORDER BY n_of_fans DESC;
