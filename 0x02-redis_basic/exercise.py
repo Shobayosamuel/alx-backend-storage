7560bf71a3ffc4572c4dce1777da3512028e9cce
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-20 04:47:58 +0000

    Read from dedis atabase

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -13,8 +13,29 @@ class Cache:
         """Initialize the object"""
         self._redis = redis.Redis()
         self._redis.flushdb()
+
     def store(self, data: Union[int, float, str, bytes]) -> str:
         """Function to store the data"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Callable[None]) -> Union[str, bytes, int, float]:
+        """Read from redis and recover the data type"""
+        data = self._redis.get(key)
+        if fn(data) is not None:
+            ans = fn(data)
+        else:
+            ans = data
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Parametize Cache.get to a str"""
+        val = self.get(key)
+        return val.decode('utf-8') if isinstance(val, bytes)
+
+    def get_int(self, key: str) -> int:
+        """Parametize Cache.get to an int"""
+        val = self.get(key)
+        return val.decode('utf-8') if isinstance(val, bytes)
