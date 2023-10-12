400284ba56b0bd3e8cd4d168be0323f502efe317
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-10-12 15:23:35 +0000

    fix: duplicate

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -8,6 +8,7 @@ BEGIN
 	SELECT AVG(score) INTO scores FROM corrections WHERE user_id=user_id;
 	INSERT INTO users(id, name, average_score)
 	VALUES (user_id, (SELECT name FROM users WHERE id=user_id), scores);
+	ON DUPLICATE KEY UPDATE average_score = scores;
 END
 //
 DELIMITER ;
