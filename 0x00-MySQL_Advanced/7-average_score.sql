0a39550fa96764245501c06f85d30b3dcc17a1ef
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-10-12 15:13:42 +0000

    compute average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,13 @@
+-- create a procedure to that computes and store average for a student
+-- An average score can be decimal
+
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	DECLARE scores DECIMAL(10, 2);
+	SELECT AVG(score) INTO scores FROM corrections WHERE user_id=user_id;
+	INSERT INTO users(id, name, average_score)
+	VALUES (user_id, (SELECT name FROM users WHERE id=user_id), scores);
+END
+//
+DELIMITER ;
