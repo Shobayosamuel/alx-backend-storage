ceb3035398ac772abffc9f1b26586b72a5520d15
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-10-13 11:29:40 +0000

    fix: changed SUM to AVG

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,7 +6,7 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
         DECLARE avg_score FLOAT;
-        SET avg_score = (SELECT SUM(score) FROM corrections WHERE corrections.user_id = user_id);
+        SET avg_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id);
         UPDATE users SET average_score=avg_score WHERE id=user_id;
 END
 //
