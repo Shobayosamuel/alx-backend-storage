dfa9ccd8517acfac568686330b2aacd54042bec7
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-10-13 13:18:40 +0000

    fix error

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,7 +1,7 @@
 -- sql script to create a view
 -- the view should containlist of all students under 80
 
-DROP TABLE IF EXISTS need_meeting;
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name FROM students
-WHERE score<80 AND (last_meeting IS ULL OR last_meeting>DATE_SUB(NOW(), INTERVAL 1 MONTH));
+WHERE score<80 AND (last_meeting IS NULL OR last_meeting<ADDDATE(CURDATE(), INTERVAL -1 MONTH));
