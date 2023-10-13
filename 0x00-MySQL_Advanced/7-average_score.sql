f9605ec346bb309f6e50198604f05e29b7a79824
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-10-13 11:15:34 +0000

    fix bug

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,13 +2,13 @@
 -- An average score can be decimal
 
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT)
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
-    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
-    UPDATE users SET average_score = avg_score WHERE id=user_id;
+        DECLARE avg_score FLOAT;
+        SELECT SUM(score) IN avg_score FROM corrections
+        WHERE user_id = user_id;
+        UPDATE users SET average_score=avg_score WHERE id=user_id;
 END
-$$
+//
 DELIMITER ;
