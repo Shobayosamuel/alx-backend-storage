751f74d82754225ae0165c9b313ed152dcc4b581
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-10-13 13:01:23 +0000

    task 11: need meeting

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,7 @@
+-- sql script to create a view
+-- the view should containlist of all students under 80
+
+DROP TABLE IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+SELECT name FROM students
+WHERE score<80 AND (last_meeting=NULL OR last_meeting>DATE_SUB(NOW(), INTERVAL 1 MONTH));
