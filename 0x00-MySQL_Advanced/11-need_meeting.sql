cc2f1fd779cc9541770a67d531d04ba27a08a0dd
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-10-13 13:10:31 +0000

    fix: syntax error

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,4 +4,4 @@
 DROP TABLE IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name FROM students
-WHERE score<80 AND (last_meeting=NULL OR last_meeting>DATE_SUB(NOW(), INTERVAL 1 MONTH));
+WHERE score<80 AND (last_meeting IS ULL OR last_meeting>DATE_SUB(NOW(), INTERVAL 1 MONTH));
